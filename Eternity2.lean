833520ebf67cce48e4eeadcb99139cd7ab656e28
James <jamesgallicchio@gmail.com>
2022-10-30 19:46:43 -0400

    Update toplevel stuff

--- Eternity2.lean
+++ Eternity2.lean
@@ -3,4 +3,5 @@ import Eternity2.Board
 import Eternity2.CardConstraints
 import Eternity2.ColorCardinality
 import Eternity2.EncCNF
+import Eternity2.SATSolve
 import Eternity2.TileSet
--- Main.lean
+++ Main.lean
@@ -2,21 +2,42 @@ import Eternity2
 
 open Eternity2
 
-def main : IO Unit := do
-  let b ← DiamondBoard.gen_dboard 2 9
+def genTileSet (size colors : Nat) : IO TileSet := do
+  let b ← DiamondBoard.gen_dboard size colors
   let t := DiamondBoard.dboard_to_tboard b false
-  IO.println t
+  return t.tileSet
 
-  let ts := t.tileSet
-  let (tsVars, enc) := EncCNF.run (do
+def fetchEternity2Tiles : IO TileSet :=
+  TileSet.fromFile "puzzles/e2pieces.txt"
+
+def signSols (ts : TileSet) : IO (List TileSet) := do
+  let (tsVars, enc) := EncCNF.new (do
     let tsVars ← ColorCardinality.colorCardConstraints ts 9
     EncCNF.addClause [⟨tsVars.head!.2, false⟩]
     return tsVars)
-  --enc.printDIMACS
 
-  match ← enc.checkSAT "rand_colorconstraints.cnf" with
-  | none => IO.println "Unsatisfiable"
-  | some as =>
-    IO.println "Satisfiable:"
-    for (t,v) in tsVars do
-      IO.println s!"{{t with sign := match as.find? v with | none => none | some true => some .plus | some false => some .minus}}"
+  let mut done := false
+  let mut count := 0
+  let mut sols := []
+
+  while !done do
+    if count % 1000 = 0 then
+      IO.println s!"count = {count}"
+    
+    match ← SATSolve.runCadical "cnf/temp.cnf" with
+    | none => done := true
+    | some as =>
+      count := count + 1
+      let sol := tsVars.map (fun (t,v) =>
+        {t with sign := as.find? v |>.map (fun | true => .plus | false => .minus)})
+      sols := sol :: sols
+      let newClause : EncCNF.Clause :=
+        tsVars.map (fun (_,v) => ⟨v, as.find? v |>.get!⟩)
+      enc.appendFileDIMACSClause "cnf/temp.cnf" newClause
+  
+  return sols
+
+
+def main : IO Unit := do
+  let ts ← genTileSet 7 7
+  TileSet.toFile "puzzles/rand7_7_7.txt" ts
