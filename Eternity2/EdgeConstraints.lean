65a0a4be491aaa7234fd3c524739bfb6dc80bc17
James <jamesgallicchio@gmail.com>
2022-11-06 13:11:41 -0500

    Implement piece classes

--- Eternity2/EdgeConstraints.lean
+++ Eternity2/EdgeConstraints.lean
@@ -12,6 +12,13 @@ structure SquareIndex (size : Nat) where
   row : Fin size
   col : Fin size
 
+inductive DiamondIndex (psize : Nat) where
+/-- col refers to the left triangle's column -/
+| horz (row : Fin psize.succ) (col : Fin psize)
+/-- row refers to the top triangle's row -/
+| vert (row : Fin psize) (col : Fin psize.succ)
+
+
 namespace SquareIndex
 
 def toFin : SquareIndex size → Fin (size * size)
@@ -30,38 +37,56 @@ private def maxIdx {psize : Nat} : Fin psize.succ := ⟨psize, Nat.lt_succ_self 
 private def middleFins (psize : Nat) : List (Fin psize.succ) :=
   forIn (m := Id) [1:psize] [] (fun x y => .yield (.ofNat x :: y))
 
-def corners (psize : Nat) : List (SquareIndex psize.succ) :=
-  [ ⟨0,0⟩
-  , ⟨0, psize, Nat.lt_succ_self _⟩
-  , ⟨maxIdx, 0⟩
-  , ⟨maxIdx, ⟨psize, Nat.lt_succ_self _⟩⟩
+def corners (psize : Nat) : List (SquareIndex psize.succ.succ × (Fin 2 → DiamondIndex psize.succ)) :=
+  [ (⟨0,0⟩, fun
+        | 0 => .horz 0 0
+        | 1 => .vert 0 0)
+  , (⟨0, maxIdx⟩, fun
+        | 0 => .vert 0 maxIdx
+        | 1 => .horz 0 maxIdx)
+  , (⟨maxIdx, 0⟩, fun
+        | 0 => .vert maxIdx 0
+        | 1 => .horz maxIdx 0)
+  , (⟨maxIdx, maxIdx⟩, fun
+        | 0 => .horz maxIdx maxIdx
+        | 1 => .vert maxIdx maxIdx)
   ]
 
-def borders (psize : Nat) : List (SquareIndex psize.succ) :=
-  middleFins psize |>.bind fun i =>
-    [ (⟨0, i⟩)
-    , (⟨i, 0⟩)
-    , (⟨maxIdx, i⟩)
-    , (⟨i, maxIdx⟩)
+def borders (psize : Nat) : List (SquareIndex psize.succ.succ × (Fin 3 → DiamondIndex psize.succ)) :=
+  middleFins psize.succ |>.bind fun i =>
+    [ (⟨0, i⟩, fun
+        | 0 => .horz 0 (.ofNat i.val)
+        | 1 => .vert 0 i
+        | 2 => .horz 0 (.ofNat (i.val-1)))
+    , (⟨i, 0⟩, fun
+        | 0 => .vert (.ofNat (i.val-1)) 0
+        | 1 => .horz i 0
+        | 2 => .vert (.ofNat i.val) 0)
+    , (⟨maxIdx, i⟩, fun
+        | 0 => .horz maxIdx (.ofNat (i.val-1))
+        | 1 => .vert maxIdx i
+        | 2 => .horz maxIdx (.ofNat i.val))
+    , (⟨i, maxIdx⟩, fun
+        | 0 => .vert (.ofNat (i.val-1)) maxIdx
+        | 1 => .horz i maxIdx
+        | 2 => .vert (.ofNat i.val) maxIdx)
     ]
 
-def center (psize : Nat) : List (SquareIndex psize.succ) :=
-  middleFins psize |>.bind fun x =>
-    middleFins psize |>.map fun y =>
-      ⟨x,y⟩
+def center (psize : Nat) : List (SquareIndex psize.succ.succ × (Fin 4 → DiamondIndex psize.succ)) :=
+  middleFins psize.succ |>.bind fun x =>
+    middleFins psize.succ |>.map fun y =>
+      (⟨x,y⟩, fun
+        | 0 => .vert (.ofNat (x.val-1)) (.ofNat y.val)
+        | 1 => .horz (.ofNat x.val) (.ofNat y.val)
+        | 2 => .vert (.ofNat x.val) (.ofNat y.val)
+        | 3 => .horz (.ofNat x.val) (.ofNat (y.val-1)))
 
 end SquareIndex
-
-inductive DiamondIndex (psize : Nat) where
-/-- col refers to the left triangle's column -/
-| horiz (row : Fin psize.succ) (col : Fin psize)
-/-- row refers to the top triangle's row -/
-| vert (row : Fin psize) (col : Fin psize.succ)
 
 namespace DiamondIndex
 
 def toFin : DiamondIndex psize → Fin (2 * (psize * psize.succ))
-| horiz ⟨i,hi⟩ ⟨j,hj⟩ => ⟨i * (psize + psize.succ) + j, by sorry⟩
+| horz ⟨i,hi⟩ ⟨j,hj⟩ => ⟨i * (psize + psize.succ) + j, by sorry⟩
 | vert ⟨i,hi⟩ ⟨j,hj⟩ => ⟨i * (psize + psize.succ) + psize + j, by sorry⟩
 
 private def maxIdx {psize : Nat} : Fin psize.succ := ⟨psize, Nat.lt_succ_self _⟩
@@ -74,15 +99,15 @@ private def minorFins (psize : Nat) : List (Fin psize) :=
 
 def border (psize : Nat) : List (DiamondIndex psize) :=
   minorFins psize |>.bind fun i =>
-    [ .horiz 0 i
-    , .horiz maxIdx i
+    [ .horz 0 i
+    , .horz maxIdx i
     , .vert i 0
     , .vert i maxIdx ]
 
 def center (psize : Nat) : List (DiamondIndex psize) :=
   middleFins psize |>.bind fun i =>
     minorFins psize |>.bind fun j =>
-      [ .horiz i j
+      [ .horz i j
       , .vert j i ]
 
 end DiamondIndex
@@ -96,21 +121,21 @@ structure TileSetVariables (psize : Nat) (colors : Nat) where
   /-- color 0 here is color 1 elsewhere -/
   diamond_vars : DiamondIndex psize → Fin colors → Var
 
-def tileIndices (psize : Nat) : List (Fin (psize.succ * psize.succ)) :=
+private def tileIndices (psize : Nat) : List (Fin (psize.succ * psize.succ)) :=
   forIn (m := Id) [0:psize.succ * psize.succ] [] (fun x y => .yield (.ofNat x :: y))
 
-def cornerTiles (tsv : TileSetVariables s c) := tileIndices s |>.filterMap (fun i =>
+private def cornerTiles (tsv : TileSetVariables s c) := tileIndices s |>.filterMap (fun i =>
   let i' : Fin tsv.ts.tiles.length := ⟨i.val, by rw [tsv.h_ts]; exact i.isLt⟩
   let tile := tsv.ts.tiles[i']
-  if tile.isCorner then some i else none)
-def borderTiles (tsv : TileSetVariables s c) := tileIndices s |>.filterMap (fun i =>
+  if tile.isCorner then some (tile,i) else none)
+private def borderTiles (tsv : TileSetVariables s c) := tileIndices s |>.filterMap (fun i =>
   let i' : Fin tsv.ts.tiles.length := ⟨i.val, by rw [tsv.h_ts]; exact i.isLt⟩
   let tile := tsv.ts.tiles[i']
-  if tile.isBorder then some i else none)
-def centerTiles (tsv : TileSetVariables s c) := tileIndices s |>.filterMap (fun i =>
+  if tile.isBorder then some (tile,i) else none)
+private def centerTiles (tsv : TileSetVariables s c) := tileIndices s |>.filterMap (fun i =>
   let i' : Fin tsv.ts.tiles.length := ⟨i.val, by rw [tsv.h_ts]; exact i.isLt⟩
   let tile := tsv.ts.tiles[i']
-  if tile.isCenter then some i else none)
+  if tile.isCenter then some (tile,i) else none)
 
 def mkVars (ts : TileSet) (psize colors : Nat)
   (h_ts : ts.tiles.length = psize.succ * psize.succ)
@@ -122,25 +147,29 @@ def mkVars (ts : TileSet) (psize colors : Nat)
   return ⟨ts, h_ts, h_colors, h_uniq, (pvs[·][·.toFin]), (dvs[·.toFin][·])⟩
 
 def pieceConstraints (tsv : TileSetVariables psize colors) : EncCNF Unit := do
+  match psize with
+  | 0 => return
+  | psize+1 =>
+
   /- Each square has a tile -/
-  for q in SquareIndex.corners psize do
-    EncCNF.addClause (cornerTiles tsv |>.map (tsv.piece_vars · q))
+  for (q,_) in SquareIndex.corners psize do
+    EncCNF.addClause (cornerTiles tsv |>.map (tsv.piece_vars ·.2 q))
 
-  for q in SquareIndex.borders psize do
-    EncCNF.addClause (borderTiles tsv |>.map (tsv.piece_vars · q))
+  for (q,_) in SquareIndex.borders psize do
+    EncCNF.addClause (borderTiles tsv |>.map (tsv.piece_vars ·.2 q))
 
-  for q in SquareIndex.center psize do
-    EncCNF.addClause (centerTiles tsv |>.map (tsv.piece_vars · q))
+  for (q,_) in SquareIndex.center psize do
+    EncCNF.addClause (centerTiles tsv |>.map (tsv.piece_vars ·.2 q))
 
   /- Each tile has a square -/
-  for p in cornerTiles tsv do
-    EncCNF.addClause (SquareIndex.corners psize |>.map (tsv.piece_vars p ·))
+  for (_,p) in cornerTiles tsv do
+    EncCNF.addClause (SquareIndex.corners psize |>.map (tsv.piece_vars p ·.1))
 
-  for p in borderTiles tsv do
-    EncCNF.addClause (SquareIndex.borders psize |>.map (tsv.piece_vars p ·))
+  for (_,p) in borderTiles tsv do
+    EncCNF.addClause (SquareIndex.borders psize |>.map (tsv.piece_vars p ·.1))
 
-  for p in centerTiles tsv do
-    EncCNF.addClause (SquareIndex.center psize |>.map (tsv.piece_vars p ·))
+  for (_,p) in centerTiles tsv do
+    EncCNF.addClause (SquareIndex.center psize |>.map (tsv.piece_vars p ·.1))
 
 def unique [DecidableEq α] (L : List α) : List α :=
   L.foldl (·.insert ·) []
@@ -175,3 +204,116 @@ def diamondConstraints (tsv : TileSetVariables psize colors) : EncCNF Unit := do
       for c' in centerColors do
         if c < c' then
           EncCNF.addClause [.not (tsv.diamond_vars d c), .not (tsv.diamond_vars d c')]
+
+/- Piece classification for essential constraints -/
+private inductive PieceClass (colors : Nat)
+| corner            (u r      : Fin colors)
+| border            (u r d    : Fin colors)
+| fourSame          (urdl     : Fin colors)
+| threeSame         (urd l    : Fin colors)
+| twoNeighborPairs  (ur dl    : Fin colors)
+| twoOppositePairs  (ud rl    : Fin colors)
+| oneNeighborPair   (ur d l   : Fin colors)
+| oneOppositePair   (ud r l   : Fin colors)
+| allDiff           (u r d l  : Fin colors)
+
+private def classify (t : Tile) (h : t.colors.all (·.all (· ≤ colors))) : PieceClass colors :=
+  /- TODO -/
+  sorry
+
+def essentialConstraints (tsv : TileSetVariables psize colors) : EncCNF Unit := do
+  match psize with
+  | 0 => return
+  | psize+1 =>
+  for _h : i in tileIndices psize.succ do
+    match
+      let i' : Fin tsv.ts.tiles.length := ⟨i.val, by rw [tsv.h_ts]; exact i.isLt⟩
+      classify (colors := colors) tsv.ts.tiles[i'] sorry
+    with
+    | .corner u r =>
+        for (q,ds) in SquareIndex.corners psize do
+          /- if i placed at q, then diamond1 colored u ∧ diamond2 colored r -/
+          EncCNF.addClause [.not (tsv.piece_vars i q), tsv.diamond_vars (ds 0) u]
+          EncCNF.addClause [.not (tsv.piece_vars i q), tsv.diamond_vars (ds 1) r]
+    | .border u r d =>
+        for (q,ds) in SquareIndex.borders psize do
+          /- if i placed at q, then diamond1 colored u ∧ diamond2 colored r ∧ diamond3 colored d -/
+          EncCNF.addClause [.not (tsv.piece_vars i q), tsv.diamond_vars (ds 0) u]
+          EncCNF.addClause [.not (tsv.piece_vars i q), tsv.diamond_vars (ds 1) r]
+          EncCNF.addClause [.not (tsv.piece_vars i q), tsv.diamond_vars (ds 2) d]
+    | .fourSame urdl =>
+        for (q,ds) in SquareIndex.center psize do
+          /- if i placed at q, then all diamonds colored urdl -/
+          for rot in [0,1,2,3] do
+            EncCNF.addClause [.not (tsv.piece_vars i q), tsv.diamond_vars (ds rot) urdl]
+    | .threeSame urd l =>
+        for (q,ds) in SquareIndex.center psize do
+          /- if i placed at q, then one diamond must be l -/
+          EncCNF.addClause [.not (tsv.piece_vars i q), tsv.diamond_vars (ds 0) l,
+            tsv.diamond_vars (ds 1) l, tsv.diamond_vars (ds 2) l, tsv.diamond_vars (ds 3) l]
+          /- and one of each opposite pair must be urd -/
+          for rot in [0,1] do
+            EncCNF.addClause [.not (tsv.piece_vars i q),
+              tsv.diamond_vars (ds rot) urd, tsv.diamond_vars (ds (rot+2)) urd]
+          /- and one of each adjacent pair must be urd -/
+          for rot in [0,1,2,3] do
+            EncCNF.addClause [.not (tsv.piece_vars i q),
+              tsv.diamond_vars (ds rot) urd, tsv.diamond_vars (ds (rot+1)) urd]
+    | .twoNeighborPairs ur dl =>
+        for (q,ds) in SquareIndex.center psize do
+          /- if i placed at q, then one of each opposite pair must be ur -/
+          for rot in [0,1] do
+            EncCNF.addClause [.not (tsv.piece_vars i q),
+              tsv.diamond_vars (ds rot) ur, tsv.diamond_vars (ds (rot+2)) ur]
+          /- and one of each opposite pair must be dl -/
+          for rot in [0,1] do
+            EncCNF.addClause [.not (tsv.piece_vars i q),
+              tsv.diamond_vars (ds rot) dl, tsv.diamond_vars (ds (rot+2)) dl]
+    | .twoOppositePairs ud rl =>
+        for (q,ds) in SquareIndex.center psize do
+          /- if i placed at q, then one of each adjacent pair must be ud -/
+          for rot in [0,1,2,3] do
+            EncCNF.addClause [.not (tsv.piece_vars i q),
+              tsv.diamond_vars (ds rot) ud, tsv.diamond_vars (ds (rot+1)) ud]
+          /- and one of each adjacent pair must be rl -/
+          for rot in [0,1,2,3] do
+            EncCNF.addClause [.not (tsv.piece_vars i q),
+              tsv.diamond_vars (ds rot) rl, tsv.diamond_vars (ds (rot+1)) rl]
+    | .oneNeighborPair ur d l =>
+        for (q,ds) in SquareIndex.center psize do
+          /- if i placed at q, then one of each opposite pair must be ur -/
+          for rot in [0,1] do
+            EncCNF.addClause [.not (tsv.piece_vars i q),
+              tsv.diamond_vars (ds rot) ur, tsv.diamond_vars (ds (rot+2)) ur]
+          /- and if the adjacent pair is rot, rot+1, then rot+2 must be d and rot+3 must be l -/
+          for rot in [0,1,2,3] do
+            EncCNF.addClause [.not (tsv.piece_vars i q),
+              .not (tsv.diamond_vars (ds rot) ur), .not (tsv.diamond_vars (ds (rot+1)) ur),
+              tsv.diamond_vars (ds (rot+2)) d]
+            EncCNF.addClause [.not (tsv.piece_vars i q),
+              .not (tsv.diamond_vars (ds rot) ur), .not (tsv.diamond_vars (ds (rot+1)) ur),
+              tsv.diamond_vars (ds (rot+3)) l]
+    | .oneOppositePair ud r l =>
+        for (q,ds) in SquareIndex.center psize do
+          /- if i placed at q, then one of each adjacent pair must be ud -/
+          for rot in [0,1,2,3] do
+            EncCNF.addClause [.not (tsv.piece_vars i q),
+              tsv.diamond_vars (ds rot) ud, tsv.diamond_vars (ds (rot+1)) ud]
+          /- and (if rot is r, rot+2 is l) and (if rot is l, rot+2 is r) -/
+          for rot in [0,1,2,3] do
+            EncCNF.addClause [.not (tsv.piece_vars i q),
+              .not (tsv.diamond_vars (ds rot) r), tsv.diamond_vars (ds (rot+2)) l]
+            EncCNF.addClause [.not (tsv.piece_vars i q),
+              .not (tsv.diamond_vars (ds rot) l), tsv.diamond_vars (ds (rot+2)) r]
+    | .allDiff u r d l =>
+        for (q,ds) in SquareIndex.center psize do
+          /- if i placed t q, then if rot is [u,r,d,l] then rot+1 is [r,d,l,u] -/
+          for rot in [0,1,2,3] do
+            EncCNF.addClause [.not (tsv.piece_vars i q),
+              .not (tsv.diamond_vars (ds rot) u), tsv.diamond_vars (ds (rot+1)) r]
+            EncCNF.addClause [.not (tsv.piece_vars i q),
+              .not (tsv.diamond_vars (ds rot) r), tsv.diamond_vars (ds (rot+1)) d]
+            EncCNF.addClause [.not (tsv.piece_vars i q),
+              .not (tsv.diamond_vars (ds rot) d), tsv.diamond_vars (ds (rot+1)) l]
+            EncCNF.addClause [.not (tsv.piece_vars i q),
+              .not (tsv.diamond_vars (ds rot) l), tsv.diamond_vars (ds (rot+1)) u]
