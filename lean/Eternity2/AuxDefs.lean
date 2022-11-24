0b20d7ae95d7bde962013f550fb1933a5dd07234
James <jamesgallicchio@gmail.com>
2022-11-24 06:55:52 -0500

    Add Log monad

--- lean/Eternity2/AuxDefs.lean
+++ lean/Eternity2/AuxDefs.lean
@@ -180,3 +180,31 @@ def randFin (n) (h : n > 0) : IO (Fin n) := do
     return ⟨i,h⟩
   else
     panic! s!"failed to get random number {i} < {n}"
+
+ 
+def Log (m) [Monad m] [MonadLiftT IO m] (α) := IO.FS.Handle → m α
+
+namespace Log
+variable {m} [Monad m] [MonadLiftT IO m]
+
+instance : Monad (Log m) where
+  pure a := fun _ => pure a
+  bind la f := fun logfile =>
+    bind (la logfile) (fun a => f a logfile)
+
+instance : MonadLift m (Log m) where
+  monadLift ma := fun _ => ma
+
+private def write (type : String) (s : String) : Log m Unit :=
+  fun logfile => do
+  let time ← (IO.monoMsNow : IO _)
+  logfile.putStrLn s!"[{time / 1000}.{time % 1000}] {type}: {s}"
+  logfile.flush
+
+def info : String → Log m Unit := write "INFO"
+def warn : String → Log m Unit := write "WARN"
+def error : String → Log m Unit := write "ERROR"
+
+def run (logfile : IO.FS.Handle) (la : Log m α) : m α := la logfile
+
+end Log
