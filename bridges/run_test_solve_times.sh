9b9cc0316c88d45a2c8ee3adaf83c04029bed59b
James <jamesgallicchio@gmail.com>
2022-11-27 22:24:22 -0500

    Make script slightly more general

--- bridges/run_test_solve_times.sh
+++ bridges/run_test_solve_times.sh
@@ -2,6 +2,11 @@
 
 BOARD_SUITE=~/eternity2/board_suite
 TIMEOUT=$1
-OUTPUT=~/log/test_times_$TIMEOUT.csv
+if [ -z "$2" ]
+then
+    OUTPUT=~/log/test_times_$TIMEOUT.csv
+else
+    OUTPUT=$2/test_times_$TIMEOUT.csv
+fi
 
 ~/eternity2/lean/build/bin/eternity2 test-solve-times --boardsuite $BOARD_SUITE --timeout $TIMEOUT > $OUTPUT
