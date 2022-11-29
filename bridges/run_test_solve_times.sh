b773129ec09adbb01f8bda4d9e430bdc1455476f
James <jamesgallicchio@gmail.com>
2022-11-29 03:22:21 -0500

    small fix for bridge script

--- bridges/run_test_solve_times.sh
+++ bridges/run_test_solve_times.sh
@@ -4,4 +4,4 @@ BOARD_SUITE=~/eternity2/board_suite
 
 OUTPUT=~/log/test_times_$1.csv
 
-~/eternity2/lean/build/bin/eternity2 test-solve-times --boardsuite $BOARD_SUITE "$@" > $OUTPUT
+~/eternity2/lean/build/bin/eternity2 test-solve-times --boardsuite $BOARD_SUITE "${@:2}" > $OUTPUT
