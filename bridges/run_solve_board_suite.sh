349aec560d4d79ddaf0ca8034929cb99f3e34cea
James <jamesgallicchio@gmail.com>
2023-03-01 20:16:53 -0500

    wrong board suite directory whoops

--- bridges/run_solve_board_suite.sh
+++ bridges/run_solve_board_suite.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BOARD_SUITE=~/eternity2/puzzle/board_suite
+BOARD_SUITE=~/eternity2/puzzles/board_suite
 LOG_FILE=~/log/solve_board_suite_$1.log
 
 ~/eternity2/lean/build/bin/eternity2 solve-board-suite --suite $BOARD_SUITE --logfile $LOG_FILE "${@:2}"
--- bridges/run_test_solve_times.sh
+++ bridges/run_test_solve_times.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-BOARD_SUITE=~/eternity2/board_suite
-
+BOARD_SUITE=~/eternity2/puzzles/board_suite
 OUTPUT=~/log/test_times_$1.csv
 
 ~/eternity2/lean/build/bin/eternity2 test-solve-times --boardsuite $BOARD_SUITE "${@:2}" > $OUTPUT
