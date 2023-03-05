dd23c0ac71d28da477603ea7960316f1a34e1d36
James <jamesgallicchio@gmail.com>
2023-03-05 04:45:20 -0500

    don't fix board suite in bridges script

--- bridges/run_solve_board_suite.sh
+++ bridges/run_solve_board_suite.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-BOARD_SUITE=~/eternity2/puzzles/board_suite
 LOG_FILE=~/log/solve_board_suite_$1.log
 
-~/eternity2/lean/build/bin/eternity2 solve-board-suite --suite $BOARD_SUITE --logfile $LOG_FILE "${@:2}"
+~/eternity2/lean/build/bin/eternity2 solve-board-suite --logfile $LOG_FILE "${@:2}"
